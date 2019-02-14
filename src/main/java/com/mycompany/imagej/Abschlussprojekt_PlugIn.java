1867e4a883477f7a4f5a0a97145f0eccdee56696
Torsten Rauch <44703626+ToRauch@users.noreply.github.com>
2019-02-14 21:47:38 +0100

    Update Abschlussprojekt_PlugIn.java

--- src/main/java/com/mycompany/imagej/Abschlussprojekt_PlugIn.java
+++ src/main/java/com/mycompany/imagej/Abschlussprojekt_PlugIn.java
@@ -36,9 +36,10 @@ public class Abschlussprojekt_PlugIn implements PlugInFilter {
     	
     	switch(whichMethod) {
         // Baustelle Torsten 1
-        case 1:
+        case 1:      	
+        	Contrast(ip);
+        	break;
         	
-            break;
         //Baustelle Torsten 2
         case 2:
         	
@@ -64,4 +65,78 @@ public class Abschlussprojekt_PlugIn implements PlugInFilter {
     
     }
   }
+
+
+
+
+
+
+
+
+
+
+
+
+	public void Contrast(ImageProcessor ip) {
+	    int M = ip.getWidth();
+	    int N = ip.getHeight();
+	    
+	    int color_min, color_max;
+	    int[] histogram;
+	    
+	    // Hard border for contrast adjustment
+	    int percentage = 5;
+	    
+	    histogram = ip.getHistogram();
+	    
+	    int minThreshold = BLACK;
+	    int maxThreshold = WHITE;  
+	    
+	
+	        // calculate amount of pixels needed to reach x%
+	        int pixelAmount = (int) Math.ceil(ip.getPixelCount() * percentage / 100.0);
+	        
+	        // the values that need to be colored in min/max values in the next step
+	        minThreshold = BLACK;
+	        maxThreshold = WHITE;	
+	        
+	        // the contrast should be expanded to the whole range of greyscales
+	        color_max = WHITE;
+	        color_min = BLACK;
+	        
+	        // the amount of pixels in the lowest/highest values
+	        int minSum = histogram[minThreshold];
+	        int maxSum = histogram[maxThreshold];
+	        
+	        // add up lowest gray shades until pixel amount needed for x% is reached
+	        while(minSum < pixelAmount) {
+	        	minThreshold++;
+	        	minSum += histogram[minThreshold];
+	        }
+	        
+	        // add up highest gray shades until pixel amount needed for x% is reached
+	        while(maxSum < pixelAmount) {
+	        	maxThreshold--;
+	        	maxSum += histogram[maxThreshold];
+	        }
+	   
+	
+	    int new_p;
+	    
+	    // iterate over all image coordinates (u,v)
+	    for (int u = 0; u < M; u++) {
+	        for (int v = 0; v < N; v++) {
+	            int p = ip.getPixel(u, v);
+	            
+	            if(p > minThreshold && p < maxThreshold) {
+	            	//scale values linear over new range
+	            	new_p = color_min + (p - minThreshold) * (color_max - color_min) / (maxThreshold - minThreshold);	
+	            } else {
+	            	new_p = (p >= maxThreshold) ? color_max : color_min;
+	            } 
+	        
+	            ip.putPixel(u, v, new_p);
+	        }
+	    }
+	}
 }
