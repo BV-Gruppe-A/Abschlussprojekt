9b1ce151a20b0b02b3e30084cfd96e3de989cb71
Torsten Rauch <44703626+ToRauch@users.noreply.github.com>
2019-02-18 23:05:13 +0100

    in klasse verschoben
    weggelöscht und in klassse getan

--- src/main/java/com/mycompany/imagej/Abschlussprojekt_PlugIn.java
+++ src/main/java/com/mycompany/imagej/Abschlussprojekt_PlugIn.java
@@ -25,11 +25,11 @@ public class Abschlussprojekt_PlugIn implements PlugInFilter {
 
     @Override    
     public int setup(String args, ImagePlus im) {  
-    	// this plugin accepts RGB images
+    	//this plugin accepts RGB images
         return DOES_RGB; 
         
     	// DEBUG FOR JANFI
-    	// return DOES_8G;
+    	//return DOES_8G;
     }
 
     @Override
@@ -40,7 +40,8 @@ public class Abschlussprojekt_PlugIn implements PlugInFilter {
     	switch(whichMethod) {
         // Baustelle Torsten 1
         case 1:      	
-        	Contrast(ip);
+        	ContrastAdjustment cont = new ContrastAdjustment();
+        	cont.Contrast(ip);
         	break;
         	
         //Baustelle Torsten 2
@@ -71,78 +72,4 @@ public class Abschlussprojekt_PlugIn implements PlugInFilter {
     
     }
   }
-
-
-
-
-
-
-
-
-
-
-
-
-	public void Contrast(ImageProcessor ip) {
-	    int M = ip.getWidth();
-	    int N = ip.getHeight();
-	    
-	    int color_min, color_max;
-	    int[] histogram;
-	    
-	    // Hard border for contrast adjustment
-	    int percentage = 5;
-	    
-	    histogram = ip.getHistogram();
-	    
-	    int minThreshold = BLACK;
-	    int maxThreshold = WHITE;  
-	    
-	
-	        // calculate amount of pixels needed to reach x%
-	        int pixelAmount = (int) Math.ceil(ip.getPixelCount() * percentage / 100.0);
-	        
-	        // the values that need to be colored in min/max values in the next step
-	        minThreshold = BLACK;
-	        maxThreshold = WHITE;	
-	        
-	        // the contrast should be expanded to the whole range of greyscales
-	        color_max = WHITE;
-	        color_min = BLACK;
-	        
-	        // the amount of pixels in the lowest/highest values
-	        int minSum = histogram[minThreshold];
-	        int maxSum = histogram[maxThreshold];
-	        
-	        // add up lowest gray shades until pixel amount needed for x% is reached
-	        while(minSum < pixelAmount) {
-	        	minThreshold++;
-	        	minSum += histogram[minThreshold];
-	        }
-	        
-	        // add up highest gray shades until pixel amount needed for x% is reached
-	        while(maxSum < pixelAmount) {
-	        	maxThreshold--;
-	        	maxSum += histogram[maxThreshold];
-	        }
-	   
-	
-	    int new_p;
-	    
-	    // iterate over all image coordinates (u,v)
-	    for (int u = 0; u < M; u++) {
-	        for (int v = 0; v < N; v++) {
-	            int p = ip.getPixel(u, v);
-	            
-	            if(p > minThreshold && p < maxThreshold) {
-	            	//scale values linear over new range
-	            	new_p = color_min + (p - minThreshold) * (color_max - color_min) / (maxThreshold - minThreshold);	
-	            } else {
-	            	new_p = (p >= maxThreshold) ? color_max : color_min;
-	            } 
-	        
-	            ip.putPixel(u, v, new_p);
-	        }
-	    }
-	}
 }
